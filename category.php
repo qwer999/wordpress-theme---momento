018a1648bd3b480c4a9c7b21a9b5b46c51b6a7fa
qwer999 <qwer999@gmail.com>
2013-07-01 17:12:29 +0900

    카테고리 뷰 수정

--- category.php
+++ category.php
@@ -17,6 +17,7 @@ get_header(); ?>
 		<div id="content" role="main">
 
 		<?php if ( have_posts() ) : ?>
+        
 			<header class="archive-header">
 				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
 
@@ -28,12 +29,12 @@ get_header(); ?>
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
-
+the_post_thumbnail( $size, $attr );
 				/* Include the post format-specific template for the content. If you want to
 				 * this in a child theme then include a file called called content-___.php
 				 * (where ___ is the post format) and that will be used instead.
 				 */
-				get_template_part( 'content', get_post_format() );
+				 the_excerpt();
 
 			endwhile;
 
