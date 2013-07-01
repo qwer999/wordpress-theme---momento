ee7ec8c3e82c94cbe389229172d62bb0be05e8c1
qwer999 <qwer999@gmail.com>
2013-07-01 17:12:40 +0900

    페이스북 좋아요 추

--- header.php
+++ header.php
@@ -22,8 +22,17 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+     
 </head>   
 <body <?php body_class(); ?> >
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/ko_KR/all.js#xfbml=1";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 	<div class="slideDiv"></div>
 	<!-- header id="masthead" class="site-header" role="banner">
 		<hgroup>
